508bd897403f5165bc01743e31985e96f8d5ddfa
zgbsss <gabrielasaraiva.04@gmail.com>
2023-08-25 10:20:57 -0300

    Reestruturação das Models

---
+++ garagem/models/acessorio.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name = "acessório"
+        verbose_name_plural = "acessórios"
