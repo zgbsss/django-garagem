6723736ec26d51c1f70e8ea95e6e2b292ae12f28
zgbsss <gabrielasaraiva.04@gmail.com>
2023-09-05 09:30:03 -0300

    Nova associação de Modelo e Veículo

--- garagem/models/modelo.py
+++ garagem/models/modelo.py
@@ -1,6 +1,9 @@
 from django.db import models
+from garagem.models import Categoria, Marca
 
 class Modelo(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="modelos")
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="modelos")
     descricao = models.CharField(max_length=50)
 
     def __str__(self):
--- garagem/models/veiculo.py
+++ garagem/models/veiculo.py
@@ -1,19 +1,17 @@
 from django.db import models
-from garagem.models import Categoria, Cor, Marca
+from garagem.models import Acessorio, Cor, Modelo
 
 class Veiculo(models.Model):
-    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
-    categoria = models.ForeignKey(
-        Categoria, on_delete=models.PROTECT, related_name="veiculos"
-    )
+    acessorio = models.ForeignKey(Acessorio, on_delete=models.PROTECT, related_name="veiculos")
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")
     ano = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(
         max_digits=10, decimal_places=2, default=0, null=True, blank=True
     )
 
     def __str__(self):
-        return f"{self.marca} - {self.categoria} - {self.ano} - {self.cor}"
+        return f"{self.modelo} - {self.ano} - {self.cor}"
 
     class Meta:
         verbose_name = "veículo"
