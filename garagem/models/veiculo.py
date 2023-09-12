5817fd188497bad9e1923933cdc9c1f99bfbb59f
zgbsss <gabrielasaraiva.04@gmail.com>
2023-09-12 08:35:04 -0300

    Criação da Característica Descrição em Veículo

--- garagem/models/veiculo.py
+++ garagem/models/veiculo.py
@@ -5,6 +5,7 @@ from uploader.models import Image
 class Veiculo(models.Model):
     acessorio = models.ManyToManyField(Acessorio, related_name="veiculos")
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    descricao = models.CharField(max_length=100, null=True, blank=True)
     modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")
     ano = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(
