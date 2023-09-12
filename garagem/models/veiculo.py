0a47743f5a7f0736113a86d5e659b2d8605253e7
zgbsss <gabrielasaraiva.04@gmail.com>
2023-09-12 08:31:04 -0300

    Adição do ManyToMany em Veículo

--- garagem/models/veiculo.py
+++ garagem/models/veiculo.py
@@ -3,14 +3,14 @@ from garagem.models import Acessorio, Cor, Modelo
 from uploader.models import Image
 
 class Veiculo(models.Model):
-    acessorio = models.ForeignKey(Acessorio, on_delete=models.PROTECT, related_name="veiculos")
+    acessorio = models.ManyToManyField(Acessorio, related_name="veiculos")
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
     modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")
     ano = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(
         max_digits=10, decimal_places=2, default=0, null=True, blank=True
     )
-    capa = models.ForeignKey(Image, related_name="+", on_delete=models.CASCADE, null=True, blank=True, default=None)
+    capa = models.ManyToManyField(Image, related_name="+", default=None)
 
     def __str__(self):
         return f"{self.modelo} - {self.ano} - {self.cor}"
